32a3519f76a4d99ba616e42d8fcecdeb403d81f9
Kuber <kuberaaganja@gmail.com>
2014-01-28 10:43:22 +0545

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoGallary::Application.config.secret_token = '8975b0cedbb05bba26a8206df7d7ef5dcf0e9e9a58683c4e4d0a4533bfad2f56ba9a7f7abe05c349f61891afe574df1cf810b50910a9751387e60c7c9c5650e5'
